9040b0fddcd6ee60081924762629840347584a9d
sheppardy22 <inv72kmaster@gmail.com>
2022-10-10 18:35:51 -0300

    Preparando Dashboard de Projetos

--- src/components/pages/Projetos.jsx
+++ src/components/pages/Projetos.jsx
@@ -1,5 +1,9 @@
 import Message from "../layout/Message";
 import { useLocation } from "react-router-dom";
+import  Container  from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+
+import Styles from './Projetos.module.css'
 
 function Projects (){
 
@@ -10,9 +14,15 @@ function Projects (){
         message = location.state.message
     }
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={Styles.project_container}>
+            <div className={Styles.tile_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to='/Novoprojeto' text='Criar Projeto' />
+            </div>
             {message && <Message type="success" msg={message} />}
+            <Container customClass ="start">
+                <p>Projetos...</p>
+            </Container>
             
         </div>
     )
